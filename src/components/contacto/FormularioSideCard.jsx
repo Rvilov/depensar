3d2681d5a82c7d0e8a88a4ccbb68bad99dacfe11
Ricardo Viloria <ricardovilov@gmail.com>
2024-08-04 10:28:49 -0400

    se corrigieron las visualizaciones en tablet

--- src/components/contacto/FormularioSideCard.jsx
+++ src/components/contacto/FormularioSideCard.jsx
@@ -1,13 +1,18 @@
 import "../../styles/contacto/formularioSideCard.css"
+
+import correo from "../../assets/Contactame/correo.png"
+import telefono from "../../assets/Contactame/telefono24.png"
 export function SideCard(){
     return( 
     <div className="formulario-info">
         <h3>Informacion de contacto</h3>
 
-        <span>
+        <span >
+        <img src={telefono} alt="" />
             +58 0424 - 7449166
         </span>
-        <span>
+        <span >
+            <img src={correo} alt="" />
             psicderlislobo@gmail.com
         </span>
        
