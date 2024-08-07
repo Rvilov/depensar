95a689d9f5ba40f931b6d3ee0ae350916f519314
Ricardo Viloria <ricardovilov@gmail.com>
2024-08-07 12:49:47 -0400

    Se mejoro responsive para tablets y se agrega direccion

--- src/components/contacto/FormularioSideCard.jsx
+++ src/components/contacto/FormularioSideCard.jsx
@@ -2,6 +2,7 @@ import "../../styles/contacto/formularioSideCard.css"
 
 import correo from "../../assets/Contactame/correo.png"
 import telefono from "../../assets/Contactame/telefono24.png"
+import direccion from "../../assets/Contactame/direccion.png"
 export function SideCard(){
     return( 
     <div className="formulario-info">
@@ -14,6 +15,10 @@ export function SideCard(){
         <span >
             <img src={correo} alt="" />
             psicderlislobo@gmail.com
+        </span>
+        <span >
+            <img src={direccion} alt="" />
+            Direccion
         </span>
        
     </div> )
